c9e3ccf0d0566943af88f5611b64424a516480ec
Erwan Jugand <erwan.jugand@gmail.com>
2020-07-01 23:15:28 +0200

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-    extends: [
-      '@nuxtjs/eslint-config-typescript'
-    ],
-    rules: {
-      'no-prototype-builtins': 'off',
-      'vue/order-in-components': 0,
-      'vue/require-default-prop': 'off',
-      'vue/require-prop-types': 'off'
-    }
+  extends: [
+    '@nuxtjs/eslint-config-typescript'
+  ],
+  rules: {
+    'no-prototype-builtins': 'off',
+    'vue/order-in-components': 0,
+    'vue/require-default-prop': 'off',
+    'vue/require-prop-types': 'off'
   }
+}
