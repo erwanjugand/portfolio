f277117702fae6cae08e99417ea251e4bc959ef9
Erwan Jugand <erwan.jugand@gmail.com>
2021-01-09 13:27:04 +0100

    refactor css

---
+++ components/PCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <component :is="tag" class="card elevation-1">
+    <div v-if="this.$slots.header" class="card-header">
+      <slot name="header" />
+    </div>
+  </component>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    tag: {
+      type: String,
+      default: ''
+    },
+  }
+})
+</script>
+
+<style lang="scss">
+.card {
+  max-width: 640px;
+  padding: 16px;
+  border-radius: 32px 8px;
+
+  &-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 16px;
+  }
+}
+</style>
