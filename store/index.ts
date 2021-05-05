eab03ea6481f8257db4bce57c5e67e2cedc5d79d
Erwan Jugand <erwan.jugand@gmail.com>
2021-05-05 22:05:09 +0200

    fix: improve type for store

--- store/index.ts
+++ store/index.ts
@@ -1,7 +1,7 @@
-import { getAccessorType } from 'typed-vuex'
-import { ActionTree, MutationTree } from 'vuex'
+import { getAccessorType, mutationTree } from 'typed-vuex'
 import { Experience, Release, Skill, Work, WorkFilter } from 'models'
 import { compareDesc, getYear } from 'date-fns'
+import slugify from '~/utils/slug'
 
 export const state = () => ({
   experiences: [] as Experience[],
@@ -12,9 +12,7 @@ export const state = () => ({
   workFilters: [] as WorkFilter[]
 })
 
-export type RootState = ReturnType<typeof state>
-
-export const mutations: MutationTree<RootState> = {
+export const mutations = mutationTree(state, {
   setExperiences (state, data: Experience[]) {
     for (const experience of data) {
       experience.dateRealization = new Date(experience.dateRealization)
@@ -22,52 +20,36 @@ export const mutations: MutationTree<RootState> = {
     }
     state.experiences = data.sort((a, b) => compareDesc(a.dateRealization, b.dateRealization))
   },
+
   setMenuOpening (state, data: boolean) {
     state.menuOpened = data
   },
+
   setReleases (state, data: Release[]) {
     for (const release of data) {
       release.date = new Date(release.date)
     }
     state.releases = data.sort((a, b) => compareDesc(a.date, b.date))
   },
+
   setSkills (state, data: Skill[]) {
     state.skills = data
   },
+
   setWorks (state, data: Work[]) {
     for (const work of data) {
       work.dateRealization = new Date(work.dateRealization)
-      work.slug = slugify(work.title)
+      work.slug = slugify(work.id + '-' + work.title)
     }
     state.works = data
   },
+
   setWorkFilters (state, data: WorkFilter[]) {
     state.workFilters = data
   }
-}
-
-export const actions: ActionTree<RootState, RootState> = {
-  // async nuxtServerInit ({ commit }, { error }) {
-  // }
-}
-
-const slugify = (text: string) => {
-  const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
-  const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnooooooooprrsssssttuuuuuuuuuwxyyzzz------'
-  const p = new RegExp(a.split('').join('|'), 'g')
-
-  return text.toString().toLowerCase()
-    .replace(/\s+/g, '-') // Replace spaces with -
-    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
-    .replace(/&/g, '-and-') // Replace & with 'and'
-    .replace(/[^\w-]+/g, '') // Remove all non-word characters
-    .replace(/--+/g, '-') // Replace multiple - with single -
-    .replace(/^-+/, '') // Trim - from start of text
-    .replace(/-+$/, '') // Trim - from end of text
-}
+})
 
 export const accessorType = getAccessorType({
   state,
-  mutations,
-  actions
+  mutations
 })
---
+++ utils/slug.ts
@@ -0,0 +1,15 @@
+
+export default function slugify (text: string) {
+  const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
+  const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnooooooooprrsssssttuuuuuuuuuwxyyzzz------'
+  const p = new RegExp(a.split('').join('|'), 'g')
+
+  return text.toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w-]+/g, '') // Remove all non-word characters
+    .replace(/--+/g, '-') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, '') // Trim - from end of text
+}
