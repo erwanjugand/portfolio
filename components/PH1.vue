58cb317a462dad727e432eaa3860293fe050fd59
Erwan Jugand <erwan.jugand@gmail.com>
2022-09-25 18:56:29 +0200

    chore: legal mentions page

---
+++ components/PH1.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="h1">
+    <slot />
+  </h1>
+</template>
+
+<style lang="scss">
+.h1 {
+  color: var(--c-primary);
+  font-size: 2.5em;
+  font-weight: $fw-bold;
+  text-align: center;
+}
+</style>
