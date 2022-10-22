e7f5d2e8c5efe55b0fbdec1675c7be45bec9a977
Erwan Jugand <erwan.jugand@gmail.com>
2022-10-22 15:08:32 +0200

    feat: add favicon

--- app.vue
+++ app.vue
@@ -12,6 +12,10 @@ useHead({
   titleTemplate: title => 'Erwan Jugand' + (title && ` - ${title}`),
   meta: [
     { name: 'viewport', content: 'width=320, initial-scale=1' }
+  ],
+  link: [
+    { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+    { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }
   ]
 })
 </script>
