bb91c2b6d265847b6053fb1eccb6753d7fd10cb1
Erwan Jugand <erwan.jugand@gmail.com>
2023-02-05 18:55:34 +0100

    clean(useDateFns)

--- composables/useDateFns.ts
+++ composables/useDateFns.ts
@@ -3,22 +3,22 @@ import { intervalToDuration, formatDuration, addMonths } from 'date-fns'
 import { fr, enGB } from 'date-fns/locale'
 import { Ref } from 'vue'
 
-export interface Locales {
+export interface DateFnsLocales {
   [key: string]: Locale
 }
 
-const locales: Locales = {
+const locales: DateFnsLocales = {
   fr,
   en: enGB
 }
 
 export default () => {
   const { currentLocale } = useLocale()
-  const locale = locales[currentLocale.value.code]
+  const dateFnsLocale = locales[currentLocale.value.code]
 
   const formatDurationDate = (startedAt: Date, finishedAt: Date): Ref<string> => {
     const interval = intervalToDuration({ start: startedAt, end: addMonths(finishedAt, 1) })
-    return ref(formatDuration(interval, { format: ['years', 'months'], locale }))
+    return ref(formatDuration(interval, { format: ['years', 'months'], locale: dateFnsLocale }))
   }
 
   return {
