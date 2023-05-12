f3e4d488b8d0da48621db9518bb6c79c4de3694d
Erwan Jugand <erwan.jugand@gmail.com>
2023-05-13 00:44:28 +0200

    feat(ci): refactor script package

---
+++ svgo.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  multipass: true,
+  js2svg: {
+    indent: 2,
+    pretty: true
+  },
+  plugins: [
+    {
+      name: 'convertPathData',
+      params: {
+        noSpaceAfterFlags: false
+      }
+    }
+  ]
+}
