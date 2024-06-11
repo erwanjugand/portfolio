e1d67fbca3484174b9d8f1920cd30236fb38e3c4
Erwan Jugand <erwan.jugand@gmail.com>
2024-06-11 20:59:39 +0200

    refactor(store): migrate to Composition Api

--- store/index.ts
+++ store/index.ts
@@ -1,43 +1,24 @@
 import { defineStore } from 'pinia'
-import { environments, type Environment } from './state/environments'
-import { experiences, type Experience } from './state/experiences'
-import { skills, type Skill } from './state/skills'
-import { releases, type Release } from './state/releases'
-import { easterEggSteps, type EasterEggStep } from './state/easter-egg'
-import { informations, type Information } from './state/informations'
+import { environments } from './state/environments'
+import { experiences } from './state/experiences'
+import { skills } from './state/skills'
+import { releases } from './state/releases'
+import { easterEggSteps } from './state/easter-egg'
+import { informations } from './state/informations'
 
-interface State {
-  environmentsList: Environment[]
-  experiencesList: Experience[]
-  skillsList: Skill[]
-  releasesList: Release[]
-  easterEggStepsList: EasterEggStep[]
-  informationsList: Information[]
-}
+export const useStore = defineStore('store', () => {
+  const lastRelease = releases[0]
+  const releasesWithTagName = (tagName: string) =>
+    releases.filter(release => release.tags.some(tag => tag.name === tagName))
 
-export const useStore = defineStore({
-  id: 'store',
-  state: (): State => {
-    return {
-      environmentsList: environments,
-      experiencesList: experiences,
-      skillsList: skills,
-      releasesList: releases,
-      easterEggStepsList: easterEggSteps,
-      informationsList: informations,
-    }
-  },
-  actions: {},
-  getters: {
-    environments: (state: State) => state.environmentsList,
-    experiences: (state: State) => state.experiencesList,
-    skills: (state: State) => state.skillsList,
-    releases: (state: State) => state.releasesList,
-    lastRelease: (state: State) => state.releasesList[0],
-    releasesWithTagName: (state: State) => (tagName: string) => {
-      return state.releasesList.filter(release => release.tags.some(tag => tag.name === tagName))
-    },
-    easterEggSteps: (state: State) => state.easterEggStepsList,
-    informations: (state: State) => state.informationsList,
-  },
+  return {
+    environments,
+    experiences,
+    skills,
+    releases,
+    lastRelease,
+    releasesWithTagName,
+    easterEggSteps,
+    informations,
+  }
 })
