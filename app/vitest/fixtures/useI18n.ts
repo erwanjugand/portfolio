460160600b2e4080bd16f231a8a7f75c2d9ed118
Erwan Jugand <erwan.jugand@gmail.com>
2024-07-27 09:35:34 +0200

    fix: ts

--- app/vitest/fixtures/useI18n.ts
+++ app/vitest/fixtures/useI18n.ts
@@ -12,7 +12,6 @@ const i18nLocaleObjects: LocaleObject[] = i18nLocales.map(locale => ({
 }))
 
 const mockUseI18n = (locale: locale) =>
-  // @ts-expect-error vue-i18n is not typed
   vi.spyOn(vueI18n, 'useI18n').mockImplementation(() => {
     return {
       locale: computed(() => locale),
