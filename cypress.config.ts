ada5fbc7a23a961687d0d4f3aa8d4351d27c5918
Erwan Jugand <erwan.jugand@gmail.com>
2024-12-14 11:09:49 +0100

    wip

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'cypress'
+// import { defineConfig } from 'cypress'
 
-export default defineConfig({
-  e2e: {
-    baseUrl: 'http://localhost:3000',
-    viewportWidth: 1280,
-    viewportHeight: 720,
-    retries: 2,
-  },
-})
+// export default defineConfig({
+//   e2e: {
+//     baseUrl: 'http://localhost:3000',
+//     viewportWidth: 1280,
+//     viewportHeight: 720,
+//     retries: 2,
+//   },
+// })
