ec88c172f6b356fedb88e94dd021d5ced95dd303
Erwan Jugand <erwan.jugand@gmail.com>
2024-12-29 12:05:10 +0100

    wip

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://localhost:3000',
+    screenshot: 'on',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -48,31 +49,11 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'pnpm run preview',
+    command: 'pnpm preview',
     port: 3000,
     reuseExistingServer: !process.env.CI,
   },
--- tests/homePage.spec.ts
+++ tests/homePage.spec.ts
@@ -7,93 +7,91 @@ test.beforeEach(async ({ page }) => {
   await page.goto('/')
 })
 
-test.describe('Home page', () => {
-  test('should have a title', async ({ page }) => {
-    const title = await page.title()
-    expect(title).toContain('Erwan Jugand')
+test('should have a title', async ({ page }) => {
+  const title = await page.title()
+  expect(title).toContain('Erwan Jugand')
 
-    const titleElement = page.getByRole('heading', { level: 1 })
-    expect(titleElement).toHaveText('Erwan Jugand')
+  const titleElement = page.getByRole('heading', { level: 1 })
+  expect(titleElement).toBeVisible()
+})
+
+test('should have a introduction section', async ({ page }) => {
+  const sectionElement = page.locator('section#introduction')
+
+  const linkElement = sectionElement.getByRole('link', { name: 'Afficher le CV' })
+  expect(linkElement).toHaveAttribute('href', '/cv')
+
+  const buttonElement = sectionElement.getByRole('button', { name: 'Défiler en bas' })
+  expect(buttonElement).toBeVisible()
+})
+
+test('should have a about section', async ({ page }) => {
+  const sectionElement = page.locator('section#about')
+
+  const titleElement = sectionElement.getByRole('heading', { level: 2, name: 'À propos' })
+  expect(titleElement).toBeVisible()
+
+  const imageElement = sectionElement.getByRole('img', { name: 'Photo de profil de Erwan Jugand' })
+  expect(imageElement).toBeVisible()
+})
+
+test('should have a skills section', async ({ page }) => {
+  const sectionElement = page.locator('section#skills')
+
+  const titleElement = sectionElement.getByRole('heading', { level: 2, name: 'Mes compétences' })
+  expect(titleElement).toBeVisible()
+
+  const skillElements = await sectionElement.getByRole('listitem').all()
+  expect(skillElements).toHaveLength(skills.length)
+
+  skillElements.forEach((skillElement, index) => {
+    expect(skillElement).toHaveAttribute('tabindex', '0')
+    expect(skillElement).toHaveText(skills[index]?.name ?? '')
   })
+})
 
-  test('should have a introduction section', async ({ page }) => {
-    const sectionElement = page.locator('section#introduction')
+test('should have a experiences section', async ({ page }) => {
+  const sectionElement = page.locator('section#experiences')
 
-    const linkElement = sectionElement.getByRole('link', { name: 'Afficher le CV' })
-    expect(linkElement).toHaveAttribute('href', '/cv')
+  const titleElement = sectionElement.getByRole('heading', { level: 2, name: 'Mes expériences' })
+  expect(titleElement).toBeVisible()
 
-    const buttonElement = sectionElement.getByRole('button', { name: 'Défiler en bas' })
-    expect(buttonElement).toBeVisible()
+  const experienceElements = await sectionElement.getByRole('listitem').all()
+  expect(experienceElements).toHaveLength(experiences.length)
+
+  experienceElements.forEach((experienceElement, index) => {
+    const titleElement = experienceElement.getByRole('heading', { level: 3 })
+    expect(titleElement).toHaveText(experiences[index]?.enterprise ?? '')
   })
+})
 
-  test('should have a about section', async ({ page }) => {
-    const sectionElement = page.locator('section#about')
+test('should have a more information section', async ({ page }) => {
+  const sectionElement = page.locator('section#more-information')
 
-    const titleElement = sectionElement.getByRole('heading', { level: 2, name: 'À propos' })
-    expect(titleElement).toBeVisible()
+  const titleElement = sectionElement.getByRole('heading', { level: 2, name: "Plus d'informations" })
+  expect(titleElement).toBeVisible()
 
-    const imageElement = sectionElement.getByRole('img', { name: 'Photo de profil de Erwan Jugand' })
-    expect(imageElement).toBeVisible()
-  })
+  const phoneLinkElement = sectionElement.getByRole('link', { name: 'Téléphone : 06 18 07 30 23' })
+  expect(phoneLinkElement).toHaveAttribute('href', 'tel:0618073023')
 
-  test('should have a skills section', async ({ page }) => {
-    const sectionElement = page.locator('section#skills')
+  const addressLinkElement = sectionElement.getByRole('link', { name: 'Adresse : 35000 Rennes, France' })
+  expect(addressLinkElement).toHaveAttribute('href', 'https://goo.gl/maps/x2VLbukCJ4L2')
 
-    const titleElement = sectionElement.getByRole('heading', { level: 2, name: 'Mes compétences' })
-    expect(titleElement).toBeVisible()
+  const emailLinkElement = sectionElement.getByRole('link', { name: 'Email : erwan.jugand@gmail.com' })
+  expect(emailLinkElement).toHaveAttribute('href', 'mailto:erwan.jugand+portfolio@gmail.com')
+})
 
-    const skillElements = await sectionElement.getByRole('listitem').all()
-    expect(skillElements).toHaveLength(skills.length)
+test('should have a environments section', async ({ page }) => {
+  const sectionElement = page.locator('section#environments')
 
-    skillElements.forEach((skillElement, index) => {
-      expect(skillElement).toHaveAttribute('tabindex', '0')
-      expect(skillElement).toHaveText(skills[index]?.name ?? '')
-    })
-  })
+  const titleElement = sectionElement.getByRole('heading', { level: 2, name: 'Mon environnement' })
+  expect(titleElement).toBeVisible()
 
-  test('should have a experiences section', async ({ page }) => {
-    const sectionElement = page.locator('section#experiences')
+  const figureElements = await sectionElement.getByRole('figure').all()
+  expect(figureElements).toHaveLength(environments.length)
 
-    const titleElement = sectionElement.getByRole('heading', { level: 2, name: 'Mes expériences' })
-    expect(titleElement).toBeVisible()
-
-    const experienceElements = await sectionElement.getByRole('listitem').all()
-    expect(experienceElements).toHaveLength(experiences.length)
-
-    experienceElements.forEach((experienceElement, index) => {
-      const titleElement = experienceElement.getByRole('heading', { level: 3 })
-      expect(titleElement).toHaveText(experiences[index]?.enterprise ?? '')
-    })
-  })
-
-  test('should have a more information section', async ({ page }) => {
-    const sectionElement = page.locator('section#more-information')
-
-    const titleElement = sectionElement.getByRole('heading', { level: 2, name: "Plus d'informations" })
-    expect(titleElement).toBeVisible()
-
-    const phoneLinkElement = sectionElement.getByRole('link', { name: 'Téléphone : 06 18 07 30 23' })
-    expect(phoneLinkElement).toHaveAttribute('href', 'tel:0618073023')
-
-    const addressLinkElement = sectionElement.getByRole('link', { name: 'Adresse : 35000 Rennes, France' })
-    expect(addressLinkElement).toHaveAttribute('href', 'https://goo.gl/maps/x2VLbukCJ4L2')
-
-    const emailLinkElement = sectionElement.getByRole('link', { name: 'Email : erwan.jugand@gmail.com' })
-    expect(emailLinkElement).toHaveAttribute('href', 'mailto:erwan.jugand+portfolio@gmail.com')
-  })
-
-  test('should have a environments section', async ({ page }) => {
-    const sectionElement = page.locator('section#environments')
-
-    const titleElement = sectionElement.getByRole('heading', { level: 2, name: 'Mon environnement' })
-    expect(titleElement).toBeVisible()
-
-    const figureElements = await sectionElement.getByRole('figure').all()
-    expect(figureElements).toHaveLength(environments.length)
-
-    figureElements.forEach((figureElement, index) => {
-      expect(figureElement).toHaveAttribute('tabindex', '0')
-      expect(figureElement).toHaveText(environments[index]?.title ?? '')
-    })
+  figureElements.forEach((figureElement, index) => {
+    expect(figureElement).toHaveAttribute('tabindex', '0')
+    expect(figureElement).toHaveText(environments[index]?.title ?? '')
   })
 })
