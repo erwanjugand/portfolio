8bad77ddf2c1bb1b428f5a1f74a6764901cb91f0
Erwan Jugand <erwan.jugand@gmail.com>
2026-05-26 19:57:55 +0200

    feat(legal-mentions): add animations

--- app/pages/legal-mentions.vue
+++ app/pages/legal-mentions.vue
@@ -25,20 +25,20 @@ defineI18nRoute({
 
       <PH2> 1. Présentation du site </PH2>
 
-      <p>
+      <p class="scroll-animation-fade">
         Merci de lire avec attention les différentes modalités d’utilisation du présent site avant d’y parcourir ses
         pages. En vous connectant sur ce site, vous acceptez sans réserves les présentes modalités. Aussi, conformément
         à l’article n°6 de la Loi n°2004-575 du 21 Juin 2004 pour la confiance dans l’économie numérique, les
         responsables du présent site internet <a href="https://erwan-jugand.fr">https://erwan-jugand.fr</a> sont :
       </p>
-      <p>Editeur du Site :</p>
-      <p>
+      <p class="scroll-animation-fade">Editeur du Site :</p>
+      <p class="scroll-animation-fade">
         Nom : Jugand<br />
         Prénom : Erwan<br />
         Email : erwan.jugand@gmail.com<br />
       </p>
-      <p>Hébergement :</p>
-      <p>
+      <p class="scroll-animation-fade">Hébergement :</p>
+      <p class="scroll-animation-fade">
         Hébergeur : Scaleway<br />
         8 rue de la Ville l’Evêque, 75008 Paris, France<br />
         Site Web : <a href="https://www.scaleway.com">https://www.scaleway.com</a>
@@ -46,14 +46,14 @@ defineI18nRoute({
 
       <PH2> 2. Conditions d’utilisation </PH2>
 
-      <p>
+      <p class="scroll-animation-fade">
         <strong>Jugand Erwan</strong> met en œuvre tous les moyens dont elle dispose, pour assurer une information
         fiable et une mise à jour fiable de ses sites internet. Toutefois, des erreurs ou omissions peuvent survenir.
         L'internaute devra donc s'assurer de l'exactitude des informations auprès de , et signaler toutes modifications
         du site qu'il jugerait utile. n'est en aucun cas responsable de l'utilisation faite de ces informations, et de
         tout préjudice direct ou indirect pouvant en découler.
       </p>
-      <p>
+      <p class="scroll-animation-fade">
         <strong>Cookies :</strong> Le site <a href="https://erwan-jugand.fr">https://erwan-jugand.fr</a> peut-être amené
         à vous demander l’acceptation des cookies pour des besoins de statistiques et d'affichage. Un cookies est une
         information déposée sur votre disque dur par le serveur du site que vous visitez. Il contient plusieurs données
@@ -61,7 +61,7 @@ defineI18nRoute({
         enregistrer des informations . Certaines parties de ce site ne peuvent être fonctionnelles sans l’acceptation de
         cookies.
       </p>
-      <p>
+      <p class="scroll-animation-fade">
         <strong>Liens hypertextes :</strong> Les sites internet de peuvent offrir des liens vers d’autres sites internet
         ou d’autres ressources disponibles sur Internet. Jugand Erwan ne dispose d'aucun moyen pour contrôler les sites
         en connexion avec ses sites internet. ne répond pas de la disponibilité de tels sites et sources externes, ni ne
@@ -70,11 +70,11 @@ defineI18nRoute({
         proposent, ou de tout usage qui peut être fait de ces éléments. Les risques liés à cette utilisation incombent
         pleinement à l'internaute, qui doit se conformer à leurs conditions d'utilisation.
       </p>
-      <p>
+      <p class="scroll-animation-fade">
         Les utilisateurs, les abonnés et les visiteurs des sites internet de ne peuvent mettre en place un hyperlien en
         direction de ce site sans l'autorisation expresse et préalable de Jugand Erwan.
       </p>
-      <p>
+      <p class="scroll-animation-fade">
         Dans l'hypothèse où un utilisateur ou visiteur souhaiterait mettre en place un hyperlien en direction d’un des
         sites internet de Jugand Erwan, il lui appartiendra d'adresser un email accessible sur le site afin de formuler
         sa demande de mise en place d'un hyperlien. Jugand Erwan se réserve le droit d’accepter ou de refuser un
@@ -83,11 +83,11 @@ defineI18nRoute({
 
       <PH2> 3. Services fournis </PH2>
 
-      <p>
+      <p class="scroll-animation-fade">
         L'ensemble des activités ainsi que ses informations sont présentés sur notre site
         <a href="https://erwan-jugand.fr">https://erwan-jugand.fr</a>.
       </p>
-      <p>
+      <p class="scroll-animation-fade">
         Jugand Erwan s’efforce de fournir sur le site https://erwan-jugand.fr des informations aussi précises que
         possible. les renseignements figurant sur le site
         <a href="https://erwan-jugand.fr">https://erwan-jugand.fr</a> ne sont pas exhaustifs et les photos non
@@ -99,14 +99,14 @@ defineI18nRoute({
 
       <PH2> 4. Limitation contractuelles sur les données </PH2>
 
-      <p>
+      <p class="scroll-animation-fade">
         Les informations contenues sur ce site sont aussi précises que possible et le site remis à jour à différentes
         périodes de l’année, mais peut toutefois contenir des inexactitudes ou des omissions. Si vous constatez une
         lacune, erreur ou ce qui parait être un dysfonctionnement, merci de bien vouloir le signaler par email, à
         l’adresse erwan.jugand@gmail.com, en décrivant le problème de la manière la plus précise possible (page posant
         problème, type d’ordinateur et de navigateur utilisé, …).
       </p>
-      <p>
+      <p class="scroll-animation-fade">
         Tout contenu téléchargé se fait aux risques et périls de l'utilisateur et sous sa seule responsabilité. En
         conséquence, ne saurait être tenu responsable d'un quelconque dommage subi par l'ordinateur de l'utilisateur ou
         d'une quelconque perte de données consécutives au téléchargement.
@@ -115,20 +115,20 @@ defineI18nRoute({
           de virus et avec un navigateur de dernière génération mis-à-jour</span
         >
       </p>
-      <p>
+      <p class="scroll-animation-fade">
         Les liens hypertextes mis en place dans le cadre du présent site internet en direction d'autres ressources
         présentes sur le réseau Internet ne sauraient engager la responsabilité de Jugand Erwan.
       </p>
 
       <PH2> 5. Propriété intellectuelle </PH2>
 
-      <p>
+      <p class="scroll-animation-fade">
         Tout le contenu du présent sur le site <a href="https://erwan-jugand.fr">https://erwan-jugand.fr</a>, incluant,
         de façon non limitative, les graphismes, images, textes, vidéos, animations, sons, logos, gifs et icônes ainsi
         que leur mise en forme sont la propriété exclusive de la Jugand Erwan à l'exception des marques, logos ou
         contenus appartenant à d'autres sociétés partenaires ou auteurs.
       </p>
-      <p>
+      <p class="scroll-animation-fade">
         Toute reproduction, distribution, modification, adaptation, retransmission ou publication, même partielle, de
         ces différents éléments est strictement interdite sans l'accord exprès par écrit de Jugand Erwan. Cette
         représentation ou reproduction, par quelque procédé que ce soit, constitue une contrefaçon sanctionnée par les
@@ -139,7 +139,7 @@ defineI18nRoute({
 
       <PH2> 6. Déclaration à la CNIL </PH2>
 
-      <p>
+      <p class="scroll-animation-fade">
         Conformément à la loi 78-17 du 6 janvier 1978 (modifiée par la loi 2004-801 du 6 août 2004 relative à la
         protection des personnes physiques à l'égard des traitements de données à caractère personnel) relative à
         l'informatique, aux fichiers et aux libertés, ce site n'a pas fait l'objet d'une déclaration auprès de la
@@ -148,7 +148,7 @@ defineI18nRoute({
 
       <PH2> 7. Litiges </PH2>
 
-      <p>
+      <p class="scroll-animation-fade">
         Les présentes conditions du site <a href="https://erwan-jugand.fr">https://erwan-jugand.fr</a> sont régies par
         les lois françaises et toute contestation ou litiges qui pourraient naître de l'interprétation ou de l'exécution
         de celles-ci seront de la compétence exclusive des tribunaux. La langue de référence, pour le règlement de
@@ -157,7 +157,7 @@ defineI18nRoute({
 
       <PH2> 8. Données personnelles </PH2>
 
-      <p>
+      <p class="scroll-animation-fade">
         De manière générale, vous n’êtes pas tenu de nous communiquer vos données personnelles lorsque vous visitez
         notre site Internet <a href="https://erwan-jugand.fr">https://erwan-jugand.fr</a>.
       </p>
